517b7bc3713805295023fcdc2c81577233a9a161
Jordan Schneidler <jschneidler@pdcbiz.com>
2018-04-27 10:43:13 -0500

    Added README. Server changes to random name from predefined list once every hour.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Discord Bot
+A modular Discord bot/system that processes and responds to chat commands
+
+## TODO
+* Create "plugin" loading
+* Add database (sqlite3 for now)
+* Abstract controllers to own modules
--- src/bot/index.js
+++ src/bot/index.js
@@ -3,22 +3,53 @@ const Discord = require('discord.js');
 const CommandController = require('./commandController');
 
 const BOT_TOKEN = process.env.BOT_TOKEN;
+const ONE_HOUR_MS = 60000 * 60;
 
 const client = new Discord.Client();
 
 client.login(BOT_TOKEN);
 
 client.on('ready', () => {
-    console.log('Discord bot logged in');
-    //console.log(`Logged in as ${client.user.tag}`);
+  console.log('Discord bot logged in');
+  //console.log(`Logged in as ${client.user.tag}`);
+  startIntervals();
 });
 
 client.on('message', msg => {
-    let action;
+  let action;
 
-    if (msg.content.startsWith('!')) {
-        const controller = new CommandController(msg);
+  if (msg.content.startsWith('!')) {
+    const controller = new CommandController(msg);
 
-        return controller.process();
-    }
+    return controller.process();
+  }
 });
+
+function startIntervals() {
+  // Random Guild Name
+  const GUILD_NAMES = [
+    'Smelly Whores',
+    'Stinky Sluts',
+    'The Flaccid Dicks',
+    'The Gooch',
+    'Ass Cheezers',
+    'Puss Plumbers',
+    'Stank Fiends',
+    'Incest Anonymous',
+    'Autofellatio Anonymous',
+    'Wankin Warriors',
+    'People With Aids',
+    'Inbred Idiots',
+    'Lost Hope Society',
+    'Cutie Cuddle Bears',
+    'Literal Fetuses',
+    'The Kids Who Got Dropped',
+  ];
+  client.setInterval(() => {
+    const randomGuildName = GUILD_NAMES[Math.floor(Math.random() * GUILD_NAMES.length)];
+    client.guilds.first().setName(randomGuildName);
+    console.log('Changed Guild name to ' + randomGuildName);
+  }, ONE_HOUR_MS);
+
+  // TODO: Random Insult
+}
